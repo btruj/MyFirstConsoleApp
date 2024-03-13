32b1ae7a9b0e796a56c34254f384ca4ed1384877
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-03-13 15:21:11 -0400

    Learning C#- added pwrd generator package and able to genrate a new password when console is ran.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PasswordGenerator;
+
+var pwd = new Password();
+var password = pwd.Next();
+Console.WriteLine(password.ToString());
